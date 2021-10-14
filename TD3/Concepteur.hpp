3da00f333b78b91f5683ab85dacdd35f88a2ec47
Mathias Gagnon <mathias.gagnon@polymtl.ca>
2021-10-14 13:33:25 -0400

    constructeur parametre, lisibilité

--- TD3/Concepteur.hpp
+++ TD3/Concepteur.hpp
@@ -6,11 +6,18 @@ class Concepteur
 public:
 	//TODO: Un constructeur par défaut et un constructeur paramétré.
 	Concepteur() = default;
+	Concepteur(const string& nom, unsigned anneNaissance, const string& pays) {
+		nom_ = nom;
+		anneeNaissance_ = anneNaissance;
+		pays_ = pays;
+	}
 
 	const std::string& getNom() const     { return nom_; }
 	void setNom(const std::string& nom)   { nom_ = nom; }
+	
 	int getAnneeNaissance() const         { return anneeNaissance_; }
 	void setAnneeNaissance(int annee)     { anneeNaissance_ = annee; }
+	
 	const std::string& getPays() const    { return pays_; }
 	void setPays(const std::string& pays) { pays_ = pays; }
 
